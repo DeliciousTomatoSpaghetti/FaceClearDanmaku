038d52470ea31a8177775f7866aca8f9c035e0a5
臧 <80999542+zang-liu-jun@users.noreply.github.com>
2025-02-27 15:24:38 +0800

    添加demo vue

---
+++ packages/demo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    hello world
+    <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+function handleClick() {
+   console.log("fuck t w") 
+}
+
+</script>
--- packages/demo/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { hello } from "danmaku";
-
-console.log(hello("world"));
